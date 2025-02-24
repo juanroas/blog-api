cebf5d841ef4592c8c5a59083dfbd37d2968c85a
juanroas <dijuanroas@gmail.com>
2025-02-23 21:45:57 -0300

    Infrastructure: EF e serviços externos (WebSockets).

---
+++ Blog/Infrastructure/Data/MySqlBlogContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Blog.Domain.Models;
+
+namespace Blog.Infrastructure.Data
+{
+    public class MySqlBlogContext : IdentityDbContext<User>
+    {
+        public MySqlBlogContext() { }
+        public MySqlBlogContext(DbContextOptions<MySqlBlogContext> options) : base(options) { }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //var config = new ConfigurationBuilder()
+                //    .SetBasePath(Directory.GetCurrentDirectory())
+                //    .AddJsonFile("appsettings.json")
+                //    .Build();
+                //var connectionString = config.GetConnectionString("MySQLConnectionString");
+
+                var connectionString = "Server=localhost;DataBase=blog_db;Uid=root;Pwd=admin123";
+
+                optionsBuilder.UseMySql(
+                       connectionString,
+                        new MySqlServerVersion(new Version(8, 4, 3)),
+                       options => options.EnableRetryOnFailure(2)
+                   );
+            }
+        }
+    }
+}
---
+++ Blog/Infrastructure/Services/NotificationService.cs
@@ -0,0 +1,69 @@
+﻿using Blog.Domain.Models;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Blog.Infrastructure.Services
+{
+    public class NotificationService
+    {
+        private readonly ConcurrentDictionary<string, WebSocket> _connections = new();
+
+        public async Task HandleWebSocketConnection(string userId, WebSocket socket)
+        {
+            _connections.TryAdd(userId, socket);
+            Console.WriteLine($"Usuário {userId} conectado ao WebSocket. Total de conexões ativas: {_connections.Count}");
+
+
+            try
+            {
+                var buffer = new byte[1024 * 4];
+                while (socket.State == WebSocketState.Open)
+                {
+                    var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        _connections.TryRemove(userId, out _);
+
+                        Console.WriteLine($"Usuário {userId} desconectado. Total de conexões ativas: {_connections.Count}");
+
+                        await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by client", CancellationToken.None);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro no WebSocket: {ex.Message}");
+                _connections.TryRemove(userId, out _);
+            }
+        }
+
+
+        public async Task NotifyNewPost(string message)
+        {
+            Console.WriteLine($"Tentando notificar {_connections.Count} usuários.");
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+
+            foreach (var (userId, socket) in _connections)
+            {
+                if (socket.State == WebSocketState.Open)
+                {
+                    await socket.SendAsync(
+                        new ArraySegment<byte>(messageBytes),
+                        WebSocketMessageType.Text,
+                        true,
+                        CancellationToken.None
+                    );
+                    Console.WriteLine($"Notificação enviada para {userId}");
+                }
+                else
+                {
+                    _connections.TryRemove(userId, out _);
+                    Console.WriteLine($"Removendo conexão inativa: {userId}");
+                }
+            }
+        }
+    }
+}
