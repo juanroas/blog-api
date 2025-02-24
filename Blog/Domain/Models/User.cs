2f6f6700abeaa47dac1c155f87d683e7735cd875
juanroas <dijuanroas@gmail.com>
2025-02-23 21:44:55 -0300

    Domain (Models): Representa as entidades do sistema.

---
+++ Blog/Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Blog.Domain.Models
+{
+    public class User : IdentityUser
+    {
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
+    }
+}
